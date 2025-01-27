fbc6959144771561334901e96e983ea7517e17d1
chris lindauer <chrislindauer@LindyPrimeAirM2.attlocal.net>
2025-01-27 14:57:00 -0500

    notated what next step in development should focus on.

--- src/deck.js
+++ src/deck.js
@@ -1,7 +1,7 @@
 
-// TODO add the shuffle functionality
 
 // this is a simple array that represents a single deck.  
+// TODO  change array to all integers.
 const deck = [
     'A', 'A', 'A', 'A', 
     2, 2, 2, 2, 
@@ -20,6 +20,8 @@ const deck = [
 
 // This equates a numerical value with the strings that represent
 //      face cards and aces.
+// TODO change const name.  
+// TODO change key value pairs to reflect new deck array
 const cardVal = {
     'K': 10,
     'Q': 10,
@@ -32,8 +34,6 @@ const shoe = [...deck, ...deck, ...deck, ...deck];
 
 // if the card has a key value pair in cardVal, it will use that value.  If not, it will use the 
         // numerical value of the card
-
-
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -65,15 +65,26 @@ deal();
 
 // console.log(playerHand);    // debug
 // console.log(bankerHand);    // debug
+
+// player gross score is calculated by adding the value of the two cards.
+// It will first check to see a card has a defined value in the cardVal object.
+// if that value exists, that is the value used.
+// if not, it will use the integer value of the card.
 let playerScore = (cardVal[playerHand[0]] || playerHand[0]) + (cardVal[playerHand[1]] || playerHand[1]);
 
-console.log(playerHand);
+console.log(playerHand); //debug
 
 // this will drop the first digit of a two digit score.
 if (playerScore > 9) {
     playerScore = JSON.parse(playerScore.toString().slice(1));
 };
 
-console.log(playerHand);
+console.log(playerHand); //debug
 
 console.log(playerScore); //debug
+
+// TODO add player scoring logic
+// TODO add banker scoring logic (dependent on player scoring)
+// TODO figure out UI for user interactions (purse, betting amt, what to bet on, etc.)
+// TODO create game loop
+// TODO (possible) arrange deck.js into either a class or a series of export functions
